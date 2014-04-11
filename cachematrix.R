9ea95c46e87d0e3c78a5ae82296b3d21918d3ba4
zhushun0008 <zhushun0008@gmail.com>
2014-04-11 17:16:54 +0800

    Version 2 This is for programming assignment 02. Implementation is done. Simple test is done and of course it may be modified later for deeply test. modified:   cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,37 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m
+  
+  list(set = set, get = get, 
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Calculate the inverse if it does not exist in the cache or the matrix has
+## been changed
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("Getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setInverse(m)
+  return(m)
 }
+
